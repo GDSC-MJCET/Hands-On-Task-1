f1e4a61e83d08be54ba7b19822524c82ad525464
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2024-01-11 10:51:27 +0530

    objective 1 complete

---
+++ src/app2.js
@@ -0,0 +1,71 @@
+const input = document.getElementById('task');
+const myForm = document.getElementById('myForm');
+const taskSection = document.getElementById('task-section');
+
+myForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if (input.value != '') {
+		createTask(input.value);
+		input.value = '';
+		input.focus();
+	}
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	if (localStorage.length) {
+		const localKeys = Object.keys(localStorage);
+		console.log(localKeys);
+		localKeys.forEach((task) => {
+			createTask(localStorage.getItem(task));
+		});
+	}
+});
+
+taskSection.addEventListener('click', (e) => {
+	const buttons = e.target.parentNode;
+	const li = buttons.parentNode;
+
+	if (e.target.classList.contains('done')) {
+		localStorage.removeItem(`task${li.dataset.taskNumber}`);
+
+		li.classList.add('bg-light');
+		li.querySelector('span').classList.add('text-decoration-line-through');
+		buttons.removeChild(e.target);
+	} else if (e.target.classList.contains('delete')) {
+		localStorage.removeItem(`task${li.dataset.taskNumber}`);
+
+		const ul = li.parentNode;
+		ul.removeChild(li);
+	}
+});
+
+function createTask(data) {
+	const li = document.createElement('li');
+	li.classList.add('task');
+
+	const doneBtn = document.createElement('button');
+	const deleteBtn = document.createElement('button');
+
+	doneBtn.classList.add('btn', 'btn-success', 'done');
+	deleteBtn.classList.add('btn', 'btn-danger', 'delete');
+
+	doneBtn.textContent = 'done';
+	deleteBtn.textContent = 'delete';
+
+	const buttons = document.createElement('div');
+	buttons.classList.add('d-flex', 'gap-2');
+
+	const span = document.createElement('span');
+	span.textContent = data;
+
+	buttons.append(doneBtn, deleteBtn);
+
+	li.append(span, buttons);
+
+	const taskNumber = taskSection.childElementCount + 1;
+	localStorage.setItem(`task${taskNumber}`, data);
+
+	li.dataset.taskNumber = taskNumber;
+
+	taskSection.appendChild(li);
+}
